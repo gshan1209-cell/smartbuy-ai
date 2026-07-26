72cb95f67932fe37ec782ac09bee79ed386f713d
Sean Lin <gshan1209@gmail.com>
2026-07-26 18:23:57 +0800

    feat: 同時註冊推薦與獎勵 Router

--- backend/api/router.py
+++ backend/api/router.py
@@ -13,6 +13,7 @@ from backend.routers.mutual_aid import router as mutual_aid_router
 from backend.routers.notifications import router as notifications_router
 from backend.routers.prediction import router as prediction_router
 from backend.routers.product import router as product_router
+from backend.routers.recommendations import router as recommendations_router
 from backend.routers.rewards import router as rewards_router
 
 ROUTERS = (
@@ -26,6 +27,7 @@ ROUTERS = (
     notifications_router,
     admin_router,
     agriculture_router,
+    recommendations_router,
     rewards_router,
 )
 
